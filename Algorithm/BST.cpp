5fd1443c6d8aab2cf61385efe0d36cf629bedfdc
GomuGomuMan <lucky7duy211@gmail.com>
2017-03-02 01:55:02 -0800

    Finished BST implementation

--- Algorithm/BST.cpp
+++ Algorithm/BST.cpp
@@ -103,11 +103,89 @@ void BST<T>::print_postorder_p(BST_Node<T> * node) const
 	print_postorder_p(node->right);
 }
 
-// Delete tree
+// Delete Node
 template <typename T>
-void BST<T>::delete_bst(BST_Node<T> * node)
+void BST<T>::delete_node(T key)
 {
-	if (node == nullptr)
-		delete root;
-	else if
+	delete_node_p(key, root);
+}
+
+template <typename T>
+BST_Node<T> * BST<T>::delete_node_p(T key, BST_Node<T> * node)
+{
+	if (!root)
+	{
+		return root;
+	}
+		
+	else if (key < node->key)
+		node->left = delete_node_p(key, node->left);
+	else if (key > node->key)
+		node->right = delete_node_p(key, node->right);
+	else
+	{
+		// Case 1: No child
+		if (!node->left && !node->right)
+		{
+			delete node;
+			node = nullptr;
+		}
+		//Case 2: 1 child
+		else if (!node->left)
+		{
+			BST_Node<T> * temp = node;
+			node = node->right;
+			delete temp;
+			temp = nullptr;
+		}
+		else if (!node->right)
+		{
+			BST_Node<T> * temp = node;
+			node = node->left;
+			delete temp;
+			temp = nullptr;
+		}
+		// Case 3: 2 children
+		else
+		{
+			BST_Node<T> * temp = find_min(node->right);
+			node->key = temp->key;
+			node->right = delete_node_p(temp->key, node->right);
+		}
+		
+	}
+
+	return node;
+}
+
+// find_min
+template <typename T>
+BST_Node<T> * BST<T>::find_min(BST_Node<T> * node) const
+{
+	BST_Node<T> * current = node;
+
+	while (current->left)
+		current = current->left;
+
+	return current;
+}
+
+/// Test
+// Delete
+template <typename T>
+void BST<T>::test_delete() 
+{
+	insert(50);
+	insert(30);
+	insert(70);
+	insert(20);
+	insert(40);
+	insert(60);
+	insert(80);
+	print_inorder();
+	std::cout << "Hello World" << std::endl;
+	delete_node(70);
+	delete_node(50);
+	delete_node(20);
+	print_inorder();
 }
--- Algorithm/BST.h
+++ Algorithm/BST.h
@@ -27,9 +27,15 @@ class BST
 		// Postorder
 		void print_postorder() const;
 
+		// Delete
+		void delete_node(T key);
+
 		// Destructor
-		/*~BST() { delete root; }*/
 		~BST() { delete root; }
+
+		/// Test
+		// Delete
+		void test_delete();
 
 	private:
 		// Insert
@@ -45,10 +51,11 @@ class BST
 		// Postorder
 		void print_postorder_p(BST_Node<T> * node) const;
 
-		// Delete tree
-		void delete_bst(BST_Node<T> * node);
+		// Delete node
+		BST_Node<T> * delete_node_p(T key, BST_Node<T> * node);
+
+		// find_min
+		BST_Node<T> * find_min(BST_Node<T> * node) const;
 
 		BST_Node<T> * root;
 };
-
-//#include "BST.cpp"
